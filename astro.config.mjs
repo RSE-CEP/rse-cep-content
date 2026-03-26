5eabc57b73309b37453a2a428c502a2ba7747695
Mat Bettinson <mat.bettinson@gmail.com>
2026-03-26 11:22:03 +1100

    Retarget Astro to new Git Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ANU-HDRH.github.io',
-  base: '/rse-cep-proto',
+  site: 'https://RSE-CEP.github.io',
+  base: '/rse-cep-content',
 });
